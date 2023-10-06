4957d5b94a27e54bc635dede4e8f1fa312a1423f
bhj8286 <bjs219@naver.com>
2023-10-06 13:05:59 +0900

    up

--- 6242_for6/sol.py
+++ 6242_for6/sol.py
@@ -1,34 +1,34 @@
-# blood_type = ['A', 'O', 'B', 'AB']
+blood_type = ['A', 'O', 'B', 'AB']
 
-# for b in blood_type:
-#     print(b)
-#     if b == blood_type[0] and b == blood_type[1]:
-#         print(3)
-#     else:
-#         print(2)
-blood_list = ['A', 'A', 'A', 'O', 'B', 'B','O', 'AB', 'AB', 'O']
-
-blood_dict = {
-    'A' : 0,
-    'O' : 0,
-    'B' : 0,
-    'AB': 0,
-}
-
-for blood in blood_list:
-    blood_dict[blood] += 1
-
-print(blood_dict)
-
-location_list = ['서울', '부산', '부산', '서울', '대전', '광주', '제주', 'LA']
-
-location_dict = {}
-
-for location in location_list:
-
-    if location in location_dict.keys():
-        location_dict[location] += 1
+for b in blood_type:
+    print(b)
+    if b == blood_type[0] or b == blood_type[1]:
+        print(3)
     else:
-        location_dict[location] = 1
+        print(2)
+# blood_list = ['A', 'A', 'A', 'O', 'B', 'B','O', 'AB', 'AB', 'O']
 
-print(location_dict)
+# blood_dict = {
+#     'A' : 0,
+#     'O' : 0,
+#     'B' : 0,
+#     'AB': 0,
+# }
+
+# for blood in blood_list:
+#     blood_dict[blood] += 1
+
+# print(blood_dict)
+
+# location_list = ['서울', '부산', '부산', '서울', '대전', '광주', '제주', 'LA']
+
+# location_dict = {}
+
+# for location in location_list:
+
+#     if location in location_dict.keys():
+#         location_dict[location] += 1
+#     else:
+#         location_dict[location] = 1
+
+# print(location_dict)
