b4dafdd28af880e8dd39a42b0211281dcdaf069a
bhj8286 <bjs219@naver.com>
2023-10-24 17:45:09 +0900

    update

--- programmers/개미군단/sol.py
+++ programmers/개미군단/sol.py
@@ -1,17 +1,35 @@
+# def solution(hp):
+#     answer = 0
+
+#     a, b = divmod(hp, 5)
+#     hp = b
+#     answer += a
+    
+#     a, b = divmod(hp, 3)
+#     hp = b
+#     answer += a
+
+#     answer += hp
+
+#     return answer
+
+
+# print(solution(23))
+# print(solution(24))
+# print(solution(999))
+
+
+
 def solution(hp):
     answer = 0
-
-    a, b = divmod(hp, 5)
-    hp = b
+    a,b = divmod(hp, 5)
     answer += a
-    
-    a, b = divmod(hp, 3)
-    hp = b
-    answer += a
-
-    answer += hp
-
+    c,d = divmod(b, 3)
+    answer += c
+    e = d // 1
+    answer += e
     return answer
+
 
 
 print(solution(23))
