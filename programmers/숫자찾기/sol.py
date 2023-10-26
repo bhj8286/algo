dbbd39997fef57aaf80e1e7172a99954e4752139
bhj8286 <bjs219@naver.com>
2023-10-26 16:52:18 +0900

    update

---
+++ programmers/숫자찾기/sol.py
@@ -0,0 +1,8 @@
+def solution(num, k):
+    answer = 0
+    return answer
+
+
+print(solution(29183, 1))
+print(solution(232443, 4))
+print(solution(123456, 7))
--- programmers/캐릭터의좌표/sol.py
+++ programmers/캐릭터의좌표/sol.py
@@ -1,21 +1,27 @@
 def solution(keyinput, board):
     meossuek = [0, 0]
     for i in range(len(keyinput)):
-        if keyinput[i] == 'left':
-            meossuek[0] -= 1
-        elif keyinput[i] == 'right':
-            meossuek[0] += 1 
-        elif keyinput[i] == 'up':
-            meossuek[1] += 1
-        elif keyinput[i] == 'down':
-            meossuek[1] -= 1
+        xlim = int((board[0]//2))
+        ylim = int((board[1]//2))
+        if keyinput[i] == 'left'and meossuek[0]-1 >= -xlim:
+             meossuek[0] -= 1
+        
+        elif keyinput[i] == 'right'and meossuek[0]+1 <= xlim:
+                meossuek[0] += 1
+        
+        elif keyinput[i] == 'up'and meossuek[1]+1 <= ylim:
+                meossuek[1] += 1
+           
+        elif keyinput[i] == 'down'and meossuek[1]-1 >= -ylim:
+                meossuek[1] -= 1
+         
+        
         else: 
-            pass
-        if abs(meossuek[0]) >= int(board[0]//2) or abs(meossuek[1]) >= int(board[1]//2):
-            break
-
-   
+            continue
+             
     return meossuek
 
 print(solution(["left", "right", "up", "right", "right"], [11, 11]))
 print(solution(["down", "down", "down", "down", "down"], [7, 9]))
+
+## abs를 이용하여 절댓값을 이용하려했지만 끝에 도달했을때 반대 방향이 나오면 실행자체가 되지 않는 경우를 고려하지 못했다.
