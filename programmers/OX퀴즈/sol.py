4b0d32af376b49077dcb0d0fbd2f96c924c69103
bhj8286 <bjs219@naver.com>
2023-11-07 23:04:09 +0900

    update

---
+++ programmers/OX퀴즈/sol.py
@@ -0,0 +1,23 @@
+def solution(quiz):
+    answer = []
+    for i in quiz:
+        my_list = list(map(str, i.split())) 
+        # list(map(str, i.split()))을 이용하여 ['3', '-', '4', '=', '-3'],['5', '+', '6', '=', '11'] 이런 식으로 리스트로 구분했다.
+        # 식이 되기 위해선 숫자와 연산자가 번갈아 등장해야 하기때문에 my_list[1]은 무조건 연산자(+ or -)라는 점을 활용했다
+        if my_list[1] == '-':
+            result = int(my_list[0])-int(my_list[2])
+            if result == int(my_list[4]):
+                answer.append('O')
+            elif result != int(my_list[4]):
+                answer.append('X')
+        elif my_list[1] == '+':
+            result = int(my_list[0])+int(my_list[2])
+            if result == int(my_list[4]):
+                answer.append('O')
+            elif result != int(my_list[4]):
+                answer.append('X')
+        
+    return answer
+
+print(solution(["3 - 4 = -3", "5 + 6 = 11"]))
+print(solution(["19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"]))
