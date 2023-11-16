b7f56f9a57b82abccef4bd45dc110a18eb604e86
bhj8286 <bjs219@naver.com>
2023-11-17 01:13:43 +0900

    update

--- programmers/LV.0/안전지대/sol.py
+++ programmers/LV.0/안전지대/sol.py
@@ -1,10 +1,28 @@
+## TC 5,6,7,11 불통, 61.1점/100점
+
 def solution(board):
     answer = 0
-    entire_board = [[0 for i in range(5)]for j in range(len(board))]
-    enire = len(board) * len(board) # => 전체보드판의 크기 
-    for k in board:
-        for num in len(board):
-            pass
+    entire_board = [[0 for i in range(len(board)+2)]for j in range(len(board)+2)]
+    entire = len(board) * len(board) # => 전체보드판의 크기
+    check = 0
+    for k in range(len(board)):
+        for h in range(len(board)):
+            if board[k][h] == 1:
+                entire_board[k][h] += 1
+                entire_board[k][h+1] += 1
+                entire_board[k][h+2] += 1
+                entire_board[k+1][h] += 1
+                entire_board[k+1][k+1] += 1
+                entire_board[k+1][k+2] += 1
+                entire_board[k+2][h] += 1
+                entire_board[k+2][h+1] += 1
+                entire_board[k+2][k+2] += 1 
+
+    for l in range(1,len(board)+1):
+        for u in range (1,len(board)+1):
+            if entire_board[l][u] >= 1:
+                check += 1
+    answer = entire - check
     return answer
    
 
---
+++ programmers/LV.1/비밀지도(1)/sol.py
@@ -0,0 +1,51 @@
+def solution(n, arr1, arr2):
+    answer = []
+    map1=[]
+    map2=[]
+    result = []
+    for i in arr1:
+        a = list(bin(i))
+        a.pop(0)
+        a.pop(0)
+        one = "".join(a)
+        map1.append(int(one))    
+    for j in arr2:
+        b = list(bin(j))
+        b.pop(0)
+        b.pop(0)
+        # if len(b) != n:
+        #     while len(b) == n:
+        #         b = b.insert(0, '0')
+       
+        two = "".join(b)
+        map2.append(int(two))
+
+    sm = [str(x+y) for x,y in zip(map1,map2)]  
+    for k in sm:
+        code = ''
+        for l in k:  
+            if l != '0':
+                l = '#'
+                code += l
+            elif l == '0':
+                l=' '
+                code += l
+        answer.append(code)
+    for g in answer:
+        g = list(g)
+        if len(g) != n:
+            while len((g)) < n:
+                g.insert(0," ")
+            g = "".join(g)
+            result.append(g)
+            
+        else:
+            g = "".join(g)
+            result.append(g)
+    
+    return result
+
+
+
+print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
+print(solution(6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10]))
