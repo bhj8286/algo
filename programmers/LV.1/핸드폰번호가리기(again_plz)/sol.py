f551f4940f5f3feaa7b74f84f0112def40045af0
bhj8286 <bjs219@naver.com>
2023-11-26 20:05:03 +0900

    update

--- programmers/LV.1/핸드폰번호가리기(again_plz)/sol.py
+++ programmers/LV.1/핸드폰번호가리기(again_plz)/sol.py
@@ -16,6 +16,42 @@ def solution(phone_number):
 print(solution("01033334444"))
 print(solution("027778888"))
 
+#첫 번째 풀이 수정버전
+def solution(phone_number):
+    answer = ''
+    pn = ''.join(phone_number)  ### 거꾸로 돌려버려서 숫자 배열자체가 달라지게 됨
+    real_num = []  
+    num_list = []
+    for _ in pn:
+        num_list.append(_)
+    real_num = num_list[:4]
+    real_num = ''.join(real_num)
+    answer = '*' * (len(phone_number)-4) + real_num
+    
+
+    return answer
+
+print(solution("01033334444"))
+print(solution("027778888"))
+
+#첫 번째 풀이 수정버전2 (reversed 살리는 버전)
+def solution(phone_number):
+    answer = ''
+    pn = ''.join(reversed(phone_number))  ### 거꾸로 돌려버려서 숫자 배열자체가 달라지게 됨
+    real_num = []  
+    num_list = []
+    for _ in pn:
+        num_list.append(_)
+    real_num = reversed(num_list[:4])
+    real_num = ''.join(real_num)
+    answer = '*' * (len(phone_number)-4) + real_num
+    
+
+    return answer
+
+print(solution("01033334444"))
+print(solution("027778888"))
+
 # 일부 수정
 def solution(phone_number):
     answer = ''
