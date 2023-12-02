0d0452b8130812c437d1633b6eb9aefb0b59cf42
bhj8286 <bjs219@naver.com>
2023-12-02 16:14:15 +0900

    update

--- programmers/LV.1/다트게임/sol.py
+++ programmers/LV.1/다트게임/sol.py
@@ -38,3 +38,37 @@ print(solution('1S*2T*3S'))
 print(solution('1D#2S*3S'))
 print(solution('1T2D3D#'))
 print(solution('1D2S3T*'))
+
+## 도현님 코드인데 re를 import해서 findall()이라는 함수를 쓴 것이 인상적이었음, 알아두면 유용하게 쓰일 듯하다
+
+import re
+# 정규 표현식 사용할 때 re 모듈 사용
+# 특정 조건의 문자열이나 패턴을 가지는 문자열을 쉽게 처리가능
+# 위치를 찾거나, 일치하는 객체를 반환, 분리, 모두 찾기 등이 가능 
+def solution(dartResult):
+    answer = []
+    power = {'S': 1, 'D': 2, 'T': 3}
+    # 해당하는 정규표현식의 문자열을 모두찾아 리스트로 반환
+    # '\d' 숫자 '+'1번이상 반복되는 패턴, '[S, D, T]' S,D,T가 있는지 찾음, [*, #] 기호 찾음
+    # '?' 앞의 패턴이 0번 혹은 1번 표시되는지 
+    a = re.findall('\d+[S, D, T][*, #]?', dartResult)
+    for i in a:
+        # 기호가 있다면 '2D*'과 같은 형식
+        if '#' in i or '*' in i:
+            # 숫자만 가져와 power 만큼 제곱해준다
+            answer.append(int(i[:-2]) ** power[i[-2]])
+            # 기호가 #이면
+            if i[-1] == '#':
+                answer[-1] = answer[-1] * (-1)
+            # 기호가 *이고 answer이 2개 이상이면
+            elif i[-1] == '*' and len(answer) >= 2:
+                answer[-1] = answer[-1] * 2
+                answer[-2] = answer[-2] * 2
+            # 기호가 *이고 answer이 1개이면
+            else:
+                answer[-1] = answer[-1] * 2
+        # 기호 없으면 제곱만 해서 저장 
+        else:
+            answer.append(int(i[:-1]) ** power[i[-1]])
+    # answer을 모두 합해 반환
+    return sum(answer)
